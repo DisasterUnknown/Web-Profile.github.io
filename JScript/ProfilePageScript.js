70727faf855ec9289c0f4d89e9e6d2912adb7c02
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-10-18 18:22:24 +0530

    profile banner part is compleated

--- JScript/ProfilePageScript.js
+++ JScript/ProfilePageScript.js
@@ -9,14 +9,13 @@ document.getElementById('age').textContent = age;
 
 // Adding music for the page 
 const vBtn = document.getElementById('volumeBtn');
+const music = document.getElementById('music');
 
 const volume = 'Resources/volume.png';
 const mute = 'Resources/mute.png';
 
 vBtn.addEventListener('click', () => {
     // Creating the music mute btn
-    const music = document.getElementById('music');
-
     if (vBtn.src.includes(volume)) {
         vBtn.src = mute;
         music.pause();
