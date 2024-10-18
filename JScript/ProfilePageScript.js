52eb60378691eee48da1914bddf9a7c06c731309
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-10-18 17:56:27 +0530

    profile page banner part compleated

--- JScript/ProfilePageScript.js
+++ JScript/ProfilePageScript.js
@@ -5,10 +5,12 @@ const age = currentYear - birthYear;
 
 document.getElementById('age').textContent = age;
 
+
+
 // Adding music for the page 
 const vBtn = document.getElementById('volumeBtn');
 
-const volume = 'Resources/volume.png'; 
+const volume = 'Resources/volume.png';
 const mute = 'Resources/mute.png';
 
 vBtn.addEventListener('click', () => {
@@ -23,4 +25,37 @@ vBtn.addEventListener('click', () => {
         music.play();
     }
 })
+
+
+
+// Changing the numbers in the number click area
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
+
+function changeNumber(num) {
+    const randomNum = Math.floor(Math.random() * 10);
+    num.textContent = randomNum;
+
+    // Checking if the numbers are 2006
+
+    const num1 = document.getElementById('num1').textContent;
+    const num2 = document.getElementById('num2').textContent;
+    const num3 = document.getElementById('num3').textContent;
+    const num4 = document.getElementById('num4').textContent;
+
+    if (num1 == '2' && num2 == '0' && num3 == '0' && num4 == '6') {
+        console.log("world");
+        
+    }
+}
+
+num1.addEventListener('click', () => { changeNumber(num1) })
+
+num2.addEventListener('click', () => { changeNumber(num2) })
+
+num3.addEventListener('click', () => { changeNumber(num3) })
+
+num4.addEventListener('click', () => { changeNumber(num4) })
 
