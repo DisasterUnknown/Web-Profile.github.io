1a6eed5f139bb5a60e3d9cd266d4de4c0d1d1806
DisasterUnknown <priyashanthafdo26@gmail.com>
2024-10-31 10:36:56 +0530

    Added the game end screen

--- JScript/GameScript.js
+++ JScript/GameScript.js
@@ -1,16 +1,27 @@
+// Refering to html element tags
 let selectOpption = document.getElementsByClassName('selectOption');
 let gameBody = document.getElementsByClassName('xox_Game');
+let gameOver = document.getElementsByClassName('gameOver');
 
 let opp1 = document.getElementById('opp1');
 let opp2 = document.getElementById('opp2');
+
+let menuBtn = document.getElementById('mainMenuBtn');
 
 let gameType = null;
 
 
 
+// Game Logic
 
 
 
+
+
+
+
+
+// game page navigation opptions
 opp1.addEventListener('click', () => {
     gameType = 1;
     
@@ -29,8 +40,18 @@ opp2.addEventListener('click', () => {
     Array.from(selectOpption).forEach(element => {
         element.style.display = 'none';
     })
-    
+
     Array.from(gameBody).forEach(element => {
         element.style.display = 'block';
+    })
+})
+
+menuBtn.addEventListener('click', () => {
+    Array.from(gameOver).forEach(element => {
+        element.style.display = 'none'
+    })
+
+    Array.from(selectOpption).forEach(element => {
+        element.style.display = 'block'
     })
 })
